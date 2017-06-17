4cff5d4e90b2c4de80d6a17bad4a2d7d745bcb24
Yanganto <yanganto@gmail.com>
2017-06-17 21:38:58 +0800

    0.0.1

--- rplugin/python/translate.py
+++ rplugin/python/translate.py
@@ -13,7 +13,78 @@ class TestPlugin(object):
     @neovim.command("Translate", range='', nargs='*')
     def translate(self, args, range):
         """Translate the current line"""
-        # TODO translate language as config
-        #self.nvim.command('echo ' + self.engin.translate(self.nvim.current.line, dest='en').text.strip())
-        self.nvim.current.line += self.engin.translate(self.nvim.current.line, dest='zh-TW').text.strip()
+        wait_for_translate = self.nvim.current.line
+        self.post_vim_message('Translating...')
 
+        # TODO translate language as config
+        self.post_vim_message(self.engin.translate(wait_for_translate, dest='zh-TW').text.strip(), warning=False)
+
+
+    # This function was refactor from YouCompleteMe vimsupport.py
+    # Calling this function from the non-GUI thread will sometimes crash Vim. At
+    # the time of writing, YCM only uses the GUI thread inside Vim (this used to
+    # not be the case).
+    def post_vim_message(self, message, warning = True, truncate = False ):
+      """Display a message on the Vim status line. By default, the message is
+      highlighted and logged to Vim command-line history (see :h history).
+      Unset the |warning| parameter to disable this behavior. Set the |truncate|
+      parameter to avoid hit-enter prompts (see :h hit-enter) when the message is
+      longer than the window width."""
+      echo_command = 'echom' if warning else 'echo'
+
+      # Displaying a new message while previous ones are still on the status line
+      # might lead to a hit-enter prompt or the message appearing without a
+      # newline so we do a redraw first.
+      self.nvim.command( 'redraw' )
+
+      if warning:
+        self.nvim.command( 'echohl WarningMsg' )
+
+      message = to_unicode( message )
+
+      if truncate:
+        self.nvim_width = get_int_value( '&columns' )
+
+        message = message.replace( '\n', ' ' )
+        if len( message ) > self.nvim_width:
+          message = message[ : self.nvim_width - 4 ] + '...'
+
+        old_ruler = get_int_value( '&ruler' )
+        old_showcmd = get_int_value( '&showcmd' )
+        self.nvim.command( 'set noruler noshowcmd' )
+
+        self.nvim.command( "{0} '{1}'".format( echo_command,
+                                         escape_for_vim( message ) ) )
+
+        SetVariableValue( '&ruler', old_ruler )
+        SetVariableValue( '&showcmd', old_showcmd )
+      else:
+        for line in message.split( '\n' ):
+          self.nvim.command( "{0} '{1}'".format( echo_command,
+                                           escape_for_vim( line ) ) )
+
+      if warning:
+        self.nvim.command( 'echohl None' )
+        
+# This function was refactor from YouCompleteMe utils.py
+# Returns a unicode type; either the new python-future str type or the real
+# unicode type. The difference shouldn't matter.
+def to_unicode( value ):
+  if not value:
+    return str()
+  if isinstance( value, str ):
+    return value
+  if isinstance( value, bytes ):
+    # All incoming text should be utf8
+    return str( value, 'utf8' )
+  return str( value )
+
+
+# This function was refactor from YouCompleteMe utils.py
+def get_int_value( variable ):
+  return int( vim.eval( variable ) )
+
+
+# This function was refactor from YouCompleteMe utils.py
+def escape_for_vim( text ):
+  return to_unicode( text.replace( "'", "''" ) )
